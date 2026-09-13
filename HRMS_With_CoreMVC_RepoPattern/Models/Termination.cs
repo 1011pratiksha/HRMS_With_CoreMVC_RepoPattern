c5cbb1457a0f484e2ffde5461a5d464d9e5f00d2
Shreya Dhole <shreyadhole131@gmail.com>
2026-09-13 14:31:16 +0530

    Added Reports model

---
+++ HRMS_With_CoreMVC_RepoPattern/Models/Termination.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HRMS_With_CoreMVC_RepoPattern.Models
+{
+    public class Termination
+    {
+        [Key]
+        public int TerminationId { get; set; }
+        
+        [ForeignKey("User")]
+        public int UserID { get; set; }
+        public string TerminationType { get; set; }
+        public DateTime NoticeDate { get; set; }
+        public DateTime ResignDate { get; set; }     
+        public string Reason { get; set; }
+        public  User User { get; set; }
+
+    }
+}
