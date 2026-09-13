6af41f0a5b898ad736e3e708a503dd193b154ba8
kusum <kusumghugare567@gmail.com>
2026-09-13 15:48:27 +0530

    Model Created

---
+++ HRMS_With_CoreMVC_RepoPattern/Models/EventTypes.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRMS_With_CoreMVC_RepoPattern.Models
+{
+    public class EventTypes
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Event Type  Name is Required.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please Select Any Color.")]
+        public string color { get; set; }
+        public List<EventModel> Event { get; set; }
+    }
+}
---
+++ HRMS_With_CoreMVC_RepoPattern/Models/Tasks.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HRMS_With_CoreMVC_RepoPattern.Models
+{
+    public class Tasks
+    {
+
+        [Key]
+        public int TaskId { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Title { get; set; }
+
+        [Required]
+        public DateTime Deadline { get; set; }
+
+        [ForeignKey("Project")]
+        public int ProjectId { get; set; }
+        public Projects Project { get; set; }
+
+
+        [Required]
+        public string Status { get; set; }
+
+
+        [Required]
+        public string Priority { get; set; }
+
+
+        [StringLength(1000)]
+        public string Description { get; set; }
+
+        public string FilePath { get; set; }
+
+        public List<TaskMembers> Taskmember { get; set; }
+
+        public List<TaskBoards> TaskBoard { get; set; }
+    }
+}
