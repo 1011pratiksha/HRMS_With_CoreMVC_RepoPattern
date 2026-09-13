9b8d6c1b611bab218ad67ecc577c97da1029caa5
Vaibhavi Girkar <vaibhavigirkar@gmail.com>
2026-09-13 16:25:02 +0530

    Add payroll leave and attendance models

---
+++ HRMS_With_CoreMVC_RepoPattern/Models/EarningType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRMS_With_CoreMVC_RepoPattern.Models
+{
+    public class EarningType
+    {
+        [Key]
+        public int EarntypeId { get; set; }
+        public string EarningName { get; set; }
+
+        public List<Earning> Earnings { get; set; }
+    }
+}
---
+++ HRMS_With_CoreMVC_RepoPattern/Models/EmployeeEarnings.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HRMS_With_CoreMVC_RepoPattern.Models
+{
+    public class EmployeeEarnings
+    {
+        [Key]
+        public int EmployeeEarningId { get; set; }
+
+        [ForeignKey("EmployeeSalaries")]
+        public int SalaryId { get; set; }
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        [ForeignKey("Earning")]
+        public int EarningId { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal EarningAmount { get; set; }
+
+        // Navigation properties
+        public virtual EmployeeSalaries EmployeeSalaries { get; set; }
+        public virtual Earning Earning { get; set; }
+    }
+}
