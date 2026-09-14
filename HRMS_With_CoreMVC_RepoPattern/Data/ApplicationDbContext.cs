01030992dcddb54bcdc3e394a0bfe6e6925486fc
Shreya Dhole <shreyadhole131@gmail.com>
2026-09-14 23:33:19 +0530

    resolves conflicts run properly

--- HRMS_With_CoreMVC_RepoPattern/Data/ApplicationDbContext.cs
+++ HRMS_With_CoreMVC_RepoPattern/Data/ApplicationDbContext.cs
@@ -102,8 +102,7 @@ namespace HRMS_With_CoreMVC_RepoPattern.Data
         public DbSet<Timesheet> Timesheet { get; set; }
         public DbSet<EmployeePerformance> EmployeePerformances { get; set; }
 
-        public DbSet<FileUpload> FileUpload { get; set; }
-        public DbSet<Timesheet> Timesheet { get; set; }
+       
 
 
     }
