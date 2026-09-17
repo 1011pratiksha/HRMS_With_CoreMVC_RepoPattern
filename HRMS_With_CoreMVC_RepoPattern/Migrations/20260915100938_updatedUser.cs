b38f4ec51b23cc678fb5f3322829a17d51f01e52
Pratiksha <jpratiksha23cs@student.mes.ac.in>
2026-09-17 20:53:39 +0530

    ignoring migrations

---
+++ HRMS_With_CoreMVC_RepoPattern/Migrations/20260915100938_updatedUser.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRMS_With_CoreMVC_RepoPattern.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Resignation_User_UserID",
+                table: "Resignation");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Termination_User_UserID",
+                table: "Termination");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_User_Role_RoleId",
+                table: "User");
+
+            migrationBuilder.RenameColumn(
+                name: "UserID",
+                table: "Termination",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Termination_UserID",
+                table: "Termination",
+                newName: "IX_Termination_UserId");
+
+            migrationBuilder.RenameColumn(
+                name: "UserID",
+                table: "Resignation",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Resignation_UserID",
+                table: "Resignation",
+                newName: "IX_Resignation_UserId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RoleId",
+                table: "User",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProfilePicture",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Gender",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateOfJoining",
+                table: "User",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "User",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AboutEmployee",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Resignation_User_UserId",
+                table: "Resignation",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Termination_User_UserId",
+                table: "Termination",
+                column: "UserId",
+                principalTable: "User",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_User_Role_RoleId",
+                table: "User",
+                column: "RoleId",
+                principalTable: "Role",
+                principalColumn: "RoleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Resignation_User_UserId",
+                table: "Resignation");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Termination_User_UserId",
+                table: "Termination");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_User_Role_RoleId",
+                table: "User");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Termination",
+                newName: "UserID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Termination_UserId",
+                table: "Termination",
+                newName: "IX_Termination_UserID");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Resignation",
+                newName: "UserID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Resignation_UserId",
+                table: "Resignation",
+                newName: "IX_Resignation_UserID");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RoleId",
+                table: "User",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProfilePicture",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Gender",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateOfJoining",
+                table: "User",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "User",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AboutEmployee",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Resignation_User_UserID",
+                table: "Resignation",
+                column: "UserID",
+                principalTable: "User",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Termination_User_UserID",
+                table: "Termination",
+                column: "UserID",
+                principalTable: "User",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_User_Role_RoleId",
+                table: "User",
+                column: "RoleId",
+                principalTable: "Role",
+                principalColumn: "RoleId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
